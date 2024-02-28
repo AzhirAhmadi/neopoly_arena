dacc713059ee390f8017300a34e73dfe0ae1e6fb
Azhir <azhir@buzzjective.com>
2024-02-28 22:50:46 +0100

    add player create end point

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class PlayersController < ApplicationController
+  def create
+    new_player = Player.new(player_params)
+
+    if new_player.save
+      render json: new_player, status: :created
+    else
+      render json: new_player.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def player_params
+    params.permit(:nickname, :password)
+  end
+end
