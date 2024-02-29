281e5b8be816988ad48502178b38628dde3782ab
Azhir <azhir@buzzjective.com>
2024-02-29 03:15:39 +0100

    add sing_in to User model

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,14 @@
 class Player < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def self.sing_in(nickname, password)
+    player = find_by(nickname: nickname, password: password)
+
+    raise 'Invalid nickname or password' unless player
+
+    player.update!(session: SecureRandom.hex, session_expires_at: 1.day.from_now)
+
+    player.session
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -8,4 +8,54 @@ RSpec.describe Player, type: :model do
   it { is_expected.to validate_presence_of(:nickname) }
   it { is_expected.to validate_uniqueness_of(:nickname) }
   it { is_expected.to validate_presence_of(:password) }
+
+  describe '#sing_in' do
+    subject(:sing_in) { described_class.sing_in(nickname, password) }
+
+    let(:player) { create(:player) }
+    let(:nickname) { player.nickname }
+    let(:password) { player.password }
+
+    context 'when the nickname and password are correct' do
+      before do |example|
+        sing_in unless example.metadata[:skip_sign_in]
+
+        player.reload
+      end
+
+      it 'returns a session', :skip_sign_in do
+        expect(sing_in).to eq(player.reload.session)
+      end
+
+      it 'updates the session' do
+        expect(player.session).to be_present
+      end
+
+      it 'updates session_expires_at' do
+        expect(player.session_expires_at).to be_present
+      end
+
+      it 'returns a session that expires in 1 day' do
+        expect(player.session_expires_at).to be_within(1.second).of(1.day.from_now)
+      end
+    end
+
+    context 'when the nickname and password are incorrect' do
+      context 'when the nickname is incorrect' do
+        let(:nickname) { 'Incorrect' }
+
+        it 'raises an error' do
+          expect { sing_in }.to raise_error('Invalid nickname or password')
+        end
+      end
+
+      context 'when the password is incorrect' do
+        let(:password) { 'Incorrect' }
+
+        it 'raises an error' do
+          expect { sing_in }.to raise_error('Invalid nickname or password')
+        end
+      end
+    end
+  end
 end
